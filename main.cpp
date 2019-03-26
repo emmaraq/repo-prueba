0bd35e11315ec18992c580db0e3aa2fbbbe8c5f8
alulab14 <alulab14@inf.pucp.edu.pe>
2019-03-26 16:00:28 -0500

    mi primer commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: alulab14
+ *
+ * Created on 26 de marzo de 2019, 03:47 PM
+ */
+
+#include <cstdlib>
+#include <cstdio>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Holi\n");
+
+    return 0;
+}
+
